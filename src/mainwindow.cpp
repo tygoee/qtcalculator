aa90b1fe77f43e301a2f8891f12670b4fc4936f2
Tygo Everts <tygoee@outlook.com>
2024-01-01 14:48:50 +0100

    Make the input work

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,10 +3,39 @@
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow) {
+    : QMainWindow(parent), ui(new Ui::MainWindow)
+{
     ui->setupUi(this);
+
+    std::initializer_list<QPushButton *> numbers = {
+        ui->pushButton_0,
+        ui->pushButton_1,
+        ui->pushButton_2,
+        ui->pushButton_3,
+        ui->pushButton_4,
+        ui->pushButton_5,
+        ui->pushButton_6,
+        ui->pushButton_7,
+        ui->pushButton_8,
+        ui->pushButton_9,
+    };
+
+    for (QPushButton *const &pushButton : numbers)
+    {
+        connect(pushButton, SIGNAL(released()), this, SLOT(digit_pressed()));
+    }
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow()
+{
     delete ui;
+}
+
+void MainWindow::digit_pressed()
+{
+    QPushButton *button = (QPushButton *)sender();
+
+    double number = (ui->label->text() + button->text()).toDouble();
+
+    ui->label->setText(QString::number(number));
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,19 +4,24 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
-   public:
+public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-   private:
+private:
     Ui::MainWindow *ui;
+
+private slots:
+    void digit_pressed();
 };
-#endif  // MAINWINDOW_H
+#endif // MAINWINDOW_H
